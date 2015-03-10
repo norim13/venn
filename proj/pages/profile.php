a8bba1e68713caac2153d1e05b8afd6f8d57a1b2
arop <andre.r.o.pires@gmail.com>
2015-03-10 23:54:33 +0000

    discarted unused files, created profile page

---
+++ proj/pages/profile.php
@@ -0,0 +1,5 @@
+<? include 'navbar.php'; ?>
+<!-- 16:9 aspect ratio -->
+<div class="embed-responsive embed-responsive-4by3">
+  <iframe class="embed-responsive-item" src="assets/img/photo.jpg"></iframe>
+</div>
