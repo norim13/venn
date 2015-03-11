9f675a743bf54f751ef555008ef4783c1641e171
Mira <fil.fmiranda@gmail.com>
2015-03-11 19:57:45 +0000

    Profile Page beta
    Adicionada profile page, é preciso ainda verificar o resize da pagina
    (nao faz resize direito, vai para além da barra lateral do lado esquerdo
    em alguns tamanhos de pagina)

--- proj/pages/profile.php
+++ proj/pages/profile.php
@@ -1,5 +1,200 @@
 <? include 'navbar.php'; ?>
-<!-- 16:9 aspect ratio -->
-<div class="embed-responsive embed-responsive-4by3">
-  <iframe class="embed-responsive-item" src="assets/img/photo.jpg"></iframe>
-</div>
+<!-- based on http://www.bootply.com/AdHpVL3xRF-->
+<div class=" page-wrapper">
+  
+	 <hr class="">
+	 <!--header profile-->
+		<div class="container target">
+			<div class="row">
+				<div class="col-sm-10">
+					<h1 class="">Mira</h1>
+					<!--
+					<button type="button" class="btn btn-success">Friend Request</button>  
+					<button type="button" class="btn btn-info">Send me a message</button>
+					-->
+					<br>
+				</div>
+			<div class="col-sm-2">
+			<!--profile pic-->
+				<img title="profile image" class="img-circle img-responsive" src="http://www.rlsandbox.com/img/profile.jpg">
+			</div>
+		</div>
+	  <br>
+  
+	<div class="row">
+		<div class="col-sm-3">
+			<!--left col-->
+			
+			<ul class="list-group">
+				<li class="list-group-item text-muted" contenteditable="false">Profile</li>
+				<li class="list-group-item text-right"><span class="pull-left"><strong class="">Joined</strong></span> 2.13.2014</li>
+				<li class="list-group-item text-right"><span class="pull-left"><strong class="">Last seen</strong></span> Yesterday</li>
+			</ul>
+			<ul class="list-group">
+				<li class="list-group-item text-muted">Activity <i class="fa fa-dashboard fa-1x"></i></li>
+				<li class="list-group-item text-right"><span class="pull-left"><strong class="">Shares</strong></span> 125</li>
+				<li class="list-group-item text-right"><span class="pull-left"><strong class="">Upvotes</strong></span> 13</li>
+				<li class="list-group-item text-right"><span class="pull-left"><strong class="">Posts</strong></span> 37</li>
+				<li class="list-group-item text-right"><span class="pull-left"><strong class="">Friends</strong></span> 78</li>
+			</ul>
+		</div>
+		
+		
+		<div class="col-sm-9" contenteditable="false" style="">
+			<!--bio collum-->
+			<div class="panel panel-default">
+				<div class="panel-heading">Mira's Bio</div>
+				<div class="panel-body">
+					<strong class="">Country</strong></span> Portugal<br/>
+					<strong class="">Sex</strong></span> Male<br/>
+					<strong class="">(...)</strong></span> (....)<br/>
+				</div>
+			</div>
+			<!--recent collum-->
+			<div class="panel panel-default target">
+			  <div class="panel-heading" contenteditable="false">Recent Posts/Reposts</div>
+				<div class="panel-body">
+				  <div class="row">
+				  
+					<div class="col-md-4">
+						<!--publication-->
+						<div class=" panel panel-default   animated  fadeInUp" id="panel-feed">
+							<!-- user's name -->
+							<div class="panel-heading ">
+								Mira
+							</div>
+
+							<div class="panel-body"  >
+								<!-- voting section -->
+								<div class="vote-section" >
+									<table style="text-align:center;">
+										<tr>
+											<td><i class="fa fa-sort-asc"></i></td>
+										</tr>
+										<tr>
+											<td>6</td>
+										</tr>
+										<tr>
+											<td><i class="fa fa-sort-desc"></i></td>
+										</tr>
+									</table>
+								</div>
+
+								<!-- publication -->
+								<p>Eux maudite pendant eparses ces facteur general ans legumes. Habitent ete fillette continue cantines galopent ils ces.
+									Nos rouge passa rirez roidi soirs <strong> (read more...)</strong></p>
+								</div> 
+
+								<!--footer-->
+								<div class="panel-footer clearfix ">
+									<!-- section with the time and comments button -->
+									<div class="fill-flow">
+										<a href="" class="btn btn-default" id="clock-panel"><i class="fa fa-clock-o"></i> 3 days ago</a>
+										<button class="btn btn-default" id="btn-comment"type="button" data-toggle="collapse" data-target="#comments" aria-expanded="false" aria-controls="comments">
+											<i class="fa fa-comments"></i> Comments
+										</button>
+									</div>
+
+									<!-- comment section-->
+									<div class="collapse" id="comments">
+										<div class="col-lg-20" id="comments_section" >
+											<h4>Comments</h4>
+											(....)
+											<br>
+											<br>
+											<!-- write a comment -->
+											<div class="input-group">
+												<textarea class="form-control" rows="1" style="resize:none"></textarea> 
+												<span class="input-group-addon btn btn-primary">Send</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						
+						
+					</div>
+					
+					<div class="col-md-4">
+					<!--publication-->
+							<div class=" panel panel-default   animated  fadeInUp" id="panel-feed">
+								<!-- user's name -->
+								<div class="panel-heading ">
+									Mira
+								</div>
+
+								<div class="panel-body"  >
+									<!-- voting section -->
+									<div class="vote-section" >
+										<table style="text-align:center;">
+											<tr>
+												<td><i class="fa fa-sort-asc"></i></td>
+											</tr>
+											<tr>
+												<td>6</td>
+											</tr>
+											<tr>
+												<td><i class="fa fa-sort-desc"></i></td>
+											</tr>
+										</table>
+									</div>
+
+									<!-- publication -->
+									<p>Eux maudite pendant eparses ces facteur general ans legumes. Habitent ete fillette continue cantines galopent ils ces.
+										</p>
+									</div> 
+
+									<!--footer-->
+									<div class="panel-footer clearfix ">
+										<!-- section with the time and comments button -->
+										<div class="fill-flow">
+											<a href="" class="btn btn-default" id="clock-panel"><i class="fa fa-clock-o"></i> 3 days ago</a>
+											<button class="btn btn-default" id="btn-comment"type="button" data-toggle="collapse" data-target="#comments" aria-expanded="false" aria-controls="comments">
+												<i class="fa fa-comments"></i> Comments
+											</button>
+										</div>
+
+										<!-- comment section-->
+										<div class="collapse" id="comments">
+											<div class="col-lg-20" id="comments_section" >
+												<h4>Comments</h4>
+												(....)
+												<br>
+												<br>
+												<!-- write a comment -->
+												<div class="input-group">
+													<textarea class="form-control" rows="1" style="resize:none"></textarea> 
+													<span class="input-group-addon btn btn-primary">Send</span>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							
+							
+						</div>
+					
+					<div class="col-md-4">
+						<div class="thumbnail">
+							<img alt="300x200" src="http://lorempixel.com/600/200/sports">
+							<div class="caption">
+								<h3>
+									Rocky
+								</h3>
+								<p>
+									Loves catnip and naps. Not fond of children.
+								</p>
+								<p>
+								
+								</p>
+							</div>
+						</div>
+				 
+					</div>
+					 
+			</div>
+				 
+		</div>
+			  
+	</div>
+	
