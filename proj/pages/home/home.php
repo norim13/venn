b5bc0f0b71fae854adff139a005827cb53d89164
arop <andre.r.o.pires@gmail.com>
2015-05-27 13:08:16 +0100

    circles in feed

--- proj/pages/home/home.php
+++ proj/pages/home/home.php
@@ -5,8 +5,11 @@ include_once '../../config/init.php';
 if($_SESSION['email']) {
     include_once '../../database/posts.php';
     include_once '../../database/users.php';
+    include_once '../../database/circles.php';
 
     $smarty->assign('recent_posts', getRecentPosts());
+    $smarty->assign('user_circles', getAllCirclesFromUser($_SESSION['id']));
+
     $smarty->display('../../templates/posts/feed.tpl');
 }
 else
